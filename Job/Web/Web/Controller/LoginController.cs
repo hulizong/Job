fd44cc427ecb97c384a5f75cd0cd7f59f6117e4e
hulizhong <a1207988466@qq.com>
2019-03-08 14:35:04 +0800

    12313

---
+++ Job/Web/Web/Controller/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Model;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+      
+        public ActionResult Index(Login login)
+        {
+
+            return View();
+        }
+
+        
+    }
+}
